38a1cce10a6b22c376103b4b8f530fa88f44cde7
Daniel Gheno <dbgheno@gmail.com>
2023-10-31 20:04:01 -0300

    Atividade 6 JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ATIVIDADE 6
 
-document.write(`ATIVIDADE 6 <br><br><br> <strong>Caso não carregue todos os exerícios, atualize a página!</strong><br><br><br>`)
+alert(`ATIVIDADE 6`)
 
 // 1. Crie uma variável para ler a idade de um motorista. Caso o motorista tenha idade maior ou igual a 18 anos imprime no console “Pode dirigir”, caso contrário imprima “Não pode dirigir”.
 
